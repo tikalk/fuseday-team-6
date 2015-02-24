dfacbb3ea1523afa2e7764bc3d59cb2dbaa31369
orizens <farhioren@gmail.com>
2015-02-24 17:45:03 +0200

    history is saved for attacks list

--- src/client/app/list/list.ctrl.js
+++ src/client/app/list/list.ctrl.js
@@ -10,10 +10,18 @@
         /*jshint validthis: true */
         var vm = this;
         vm.title = 'ListCtrl';
+        vm._attacks = attacks.items;
         vm.attacks = attacks.items;
         activate();
 
         function activate() {
+            $scope.$watch('vm._attacks', function (items) {
+                if (items && items.length) {
+                    items.forEach(function(item){
+                        vm.attacks.push(item);
+                    });
+                }
+            });
             attacks.start();
         }
     }
